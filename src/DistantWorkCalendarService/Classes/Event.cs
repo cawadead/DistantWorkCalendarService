d8e02d007d09331f1d9dadf6688a8e183bce3a8b
Alexey <savelyev.main@gmail.com>
2024-07-17 09:57:26 +0600

    модель данных EventStatus и контекст БД

--- src/DistantWorkCalendarService/Classes/Event.cs
+++ src/DistantWorkCalendarService/Classes/Event.cs
@@ -5,11 +5,9 @@ namespace DistantWorkCalendarService.Classes
     public class Event
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public EEventType Type { get; set; }
-        [JsonPropertyName("start")]
-        public DateTime StartDate {  get; set; }
-        [JsonPropertyName("end")]
-        public DateTime EndDate { get; set; }
+        public ICollection<EventStatus> EventStatuses { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted {  get; set; }
     }
 }
